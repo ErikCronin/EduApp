e5c479b4f6be409a9ba0db24aad40d64c787cfdd
ErikCronin <erik.cronin@my.jcu.edu.au>
2020-05-15 10:39:25 +1000

    initial commit

---
+++ app/src/main/java/com/erik/eduapp/GameActivity.java
@@ -0,0 +1,35 @@
+package com.erik.eduapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        setupDescription(R.id.task1, R.array.task1_descriptions);
+        setupDescription(R.id.task2, R.array.task2_descriptions);
+
+        for(int l=0; l<=4; l++){
+            addImage();
+        }
+    }
+
+    private void addImage() {
+        ViewGroup gameRows = findViewById(R.id.game_rows);
+        getLayoutInflater().inflate(R.layout.image, gameRows);
+
+        View lastChild = gameRows.getChildAt(gameRows.getChildCount() - 1);
+        ImageView image = lastChild.findViewById(R.id.image);
+
+        int index = random.nextInt(drawables.length);
+        image.setImageDrawable(getResources().getDrawable(drawables[index]));
+    }
+}
